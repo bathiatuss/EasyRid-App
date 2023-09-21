619d4352968815c499f64dcc3219b0fe23c82d39
Atakan <atakanbaltac-1@hotmail.com>
2023-09-21 11:15:19 +0300

    AppText.js added

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from "react";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
+import AppText from "./app/components/AppText";
+
 function App(props) {
   return <ViewImageScreen />;
 }
