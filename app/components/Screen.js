ed9ec9fc390fe2ec0c142c6728a19dadf281445e
Atakan <atakanbaltac-1@hotmail.com>
2023-09-27 22:36:46 +0300

    listingscreen flatlist added

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -10,13 +10,18 @@ import React from "react";
 import colors from "../config/colors";
 
 function Screen({ children, style }) {
-  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>;
+  return (
+    <SafeAreaView style={[styles.screen, style]}>
+      <View style={style}>{children}</View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   screen: {
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0, //expo constants statusbarheight
     flex: 1,
+    backgroundColor: "#f8f4f4",
   },
 });
 
--- app/screens/ListingScreen.js
+++ app/screens/ListingScreen.js
@@ -1,23 +1,43 @@
 import React from "react";
-import { View, StyleSheet } from "react-native-web";
+import { View, StyleSheet } from "react-native";
 import Screen from "../components/Screen";
 import Card from "../components/Card";
+import { FlatList } from "react-native";
+import colors from "../config/colors";
+
+const listings = [
+  {
+    id: 1,
+    title: "Red jacket for sale",
+    price: 100,
+    image: require("../assets/jacket.jpg"),
+  },
+  {
+    id: 2,
+    title: "Couch in great condition",
+    price: 1000,
+    image: require("../assets/couch.jpg"),
+  },
+];
 
 function ListingScreen(props) {
   return (
-    <Screen>
-      <Card
-        title={"Red jacket for sale"}
-        subTitle={"$100"}
-        image={require("../assets/jacket.jpg")}
-      />
-      <Card
-        title={"Couch in great condition"}
-        subTitle={"$1000"}
-        image={require("../assets/couch.jpg")}
+    <Screen style={styles.screen}>
+      <FlatList
+        data={listings}
+        keyExtractor={(listings) => listings.id.toString()}
+        renderItem={({ item }) => (
+          <Card
+            title={item.title}
+            subTitle={"$" + item.price}
+            image={item.image}
+          />
+        )}
       />
     </Screen>
   );
 }
+
+const styles = StyleSheet.create({});
 
 export default ListingScreen;
