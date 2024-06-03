23bfdb5b617c1d059e7979e2af41c451167f7b03
Atakan <atakanbaltac-1@hotmail.com>
2024-06-04 00:47:54 +0300

    tabs & stack navigators created

--- App.js
+++ App.js
@@ -1,60 +1,91 @@
-import React, { useEffect, useState } from "react";
-// import { Text } from "react-native";
-// import { TextInput, Switch } from "react-native";
+import { Text } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+// import * as ImagePicker from "expo-image-picker";
+// import * as Permisions from "expo-permissions";
 
+// import colors from "./app/config/colors";
 // import Icon from "./app/components/Icon";
-import Screen from "./app/components/Screen";
+import AppButton from "./app/components/AppButton";
 // import AppText from "./app/components/AppText";
 // import Card from "./app/components/Card";
 // import ListItem from "./app/components/ListItem";
+// import AppFormPicker from "./app/components/forms/AppFormPicker";
 // import AppTextInput from "./app/components/AppTextInput";
-import * as ImagePicker from "expo-image-picker";
-import * as Permisions from "expo-permissions";
-import { Button, Image } from "react-native";
-import ImageInput from "./app/components/ImageInput";
-import ImageInputList from "./app/components/ImageInputList";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+// import AppPicker from "./app/components/AppPicker";
+// import ImageInput from "./app/components/ImageInput";
+// import ImageInputList from "./app/components/ImageInputList";
 
+import Screen from "./app/components/Screen";
+// import ListingEditScreen from "./app/screens/ListingEditScreen";
 // import WelcomeScreen from "./app/screens/WelcomeScreen";
 // import ViewImageScreen from "./app/screens/ViewImageScreen";
 // import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 // import MessagesScreen from "./app/screens/MessagesScreen";
 // import AccountScreen from "./app/screens/AccountScreen";
 // import ListingScreen from "./app/screens/ListingScreen";
-
-// import colors from "./app/config/colors";
-// import AppPicker from "./app/components/AppPicker";
 // import LoginScreen from "./app/screens/LoginScreen";
 // import RegisterScreen from "./app/screens/RegisterScreen";
-// import AppFormPicker from "./app/components/forms/AppFormPicker";
 // import ListingEditScreen from "./app/screens/ListingEditScreen";
 
-const Tweets = () => (
+const Tweets = ({ navigation }) => (
   <Screen>
     <Text>Tweets</Text>
+    <AppButton
+      title="View Tweet"
+      onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
+    />
   </Screen>
 );
 
-const TweetDetails = () => (
+const TweetDetails = ({ route }) => (
   <Screen>
-    <Text>Tweet Details</Text>
+    <Text>Tweet Details {route.params.id}</Text>
   </Screen>
 );
 
 const Stack = createNativeStackNavigator();
+
 const StackNavigator = () => (
-  <StackNavigator>
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: "dodgerblue" },
+      headerTintColor: "white",
+    }}
+  >
     <Stack.Screen name="Tweets" component={Tweets} />
-    <Stack.Screen name="TweetDetails" component={TweetDetails} />
-  </StackNavigator>
+    <Stack.Screen
+      name="TweetDetails"
+      component={TweetDetails}
+      options={{
+        headerStyle: { backgroundColor: "tomato" },
+        headerTintColor: "white",
+      }}
+    />
+  </Stack.Navigator>
 );
 
-export default function App(props) {
+const Account = () => (
+  <Screen>
+    <Text>Account</Text>
+  </Screen>
+);
+
+/*Feed tab name still on the top as a header. bug!*/
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Feed" component={StackNavigator} />
+    <Tab.Screen name="Account" component={Account} />
+  </Tab.Navigator>
+);
+
+export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <TabNavigator />
     </NavigationContainer>
   );
 }
